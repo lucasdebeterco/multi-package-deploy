9b532cc91bf48bcbb587296dfb3fdd404b83162b
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2024-11-05 23:29:56 -0300

    components test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { Menubar } from "@multi-package-deploy/menubar"
+import { Button } from "@multi-package-deploy/button"
+import { Input } from "@multi-package-deploy/input"
+
 function App() {
 
   return (
-    <div className='bg-red-500'>
-     aaa
-    </div>
+      <div className='p-20 max-w-[1280px] mx-auto flex flex-col gap-10'>
+          <Button />
+          aa
+          <Input />
+          aa
+          <Menubar />
+      </div>
   )
 }
 
--- src/packages/button/src/button.tsx
+++ src/packages/button/src/button.tsx
@@ -1,5 +1,5 @@
 export function Button() {
-    <>
+    <div>
         Button
-    </>
+    </div>
 }
