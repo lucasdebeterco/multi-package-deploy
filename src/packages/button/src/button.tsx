ac9fcf327e88cefa95fc2e86f802b6cdcfba54e1
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2024-11-16 23:20:35 -0300

    styling

--- src/packages/button/src/button.tsx
+++ src/packages/button/src/button.tsx
@@ -1,6 +1,6 @@
 export function Button() {
     return (
-        <button className='w-full py-2 bg-violet-900 text-white rounded-md'>
+        <button className='w-full py-2 bg-violet-900 text-white rounded-md hover:bg-violet-900/80 transition-colors'>
             Button
         </button>
     )
--- src/packages/menubar/src/menubar.tsx
+++ src/packages/menubar/src/menubar.tsx
@@ -14,13 +14,13 @@ export function MenubarDemo() {
                         sideOffset={5}
                         alignOffset={-3}
                     >
-                        <Menubar.Item className="group relative flex h-[25px] select-none items-center rounded px-2.5 leading-none text-violet-400 outline-none data-[disabled]:pointer-events-none data-[state=open]:bg-violet-50 data-[highlighted]:bg-gradient-to-br data-[highlighted]:from-violet-500 data-[highlighted]:to-violet-800 data-[disabled]:text-zinc-600 data-[highlighted]:data-[state=open]:text-violet-400 data-[highlighted]:text-violet-400 data-[state=open]:text-violet-800">
+                        <Menubar.Item className="group relative flex h-[34px] select-none items-center rounded px-2.5 leading-none text-zinc-100 outline-none data-[disabled]:pointer-events-none data-[state=open]:bg-violet-50 data-[highlighted]:bg-gradient-to-br data-[highlighted]:from-violet-500 data-[highlighted]:to-violet-800 data-[disabled]:text-zinc-600 data-[highlighted]:data-[state=open]:text-zinc-100 data-[highlighted]:text-zinc-100 data-[state=open]:text-violet-800">
                             New Tab{" "}
                         </Menubar.Item>
-                        <Menubar.Item className="group relative flex h-[25px] select-none items-center rounded px-2.5 leading-none text-violet-400 outline-none data-[disabled]:pointer-events-none data-[state=open]:bg-violet-50 data-[highlighted]:bg-gradient-to-br data-[highlighted]:from-violet-500 data-[highlighted]:to-violet-800 data-[disabled]:text-zinc-600 data-[highlighted]:data-[state=open]:text-violet-400 data-[highlighted]:text-violet-400 data-[state=open]:text-violet-800">
+                        <Menubar.Item className="group relative flex h-[34px] select-none items-center rounded px-2.5 leading-none text-zinc-100 outline-none data-[disabled]:pointer-events-none data-[state=open]:bg-violet-50 data-[highlighted]:bg-gradient-to-br data-[highlighted]:from-violet-500 data-[highlighted]:to-violet-800 data-[disabled]:text-zinc-600 data-[highlighted]:data-[state=open]:text-zinc-100 data-[highlighted]:text-zinc-100 data-[state=open]:text-violet-800">
                             New Window{" "}
                         </Menubar.Item>
-                        <Menubar.Item className="relative flex h-[25px] select-none items-center rounded px-2.5 leading-none text-violet-400 outline-none data-[disabled]:pointer-events-none data-[state=open]:bg-violet-50 data-[highlighted]:bg-gradient-to-br data-[highlighted]:from-violet-900 data-[highlighted]:to-violet-900 data-[disabled]:text-zinc-500 data-[highlighted]:data-[state=open]:text-violet-950 data-[highlighted]:text-violet-950 data-[state=open]:text-violet-900">
+                        <Menubar.Item className="group relative flex h-[34px] select-none items-center rounded px-2.5 leading-none text-zinc-100 outline-none data-[disabled]:pointer-events-none data-[state=open]:bg-violet-50 data-[highlighted]:bg-gradient-to-br data-[highlighted]:from-violet-500 data-[highlighted]:to-violet-800 data-[disabled]:text-zinc-600 data-[highlighted]:data-[state=open]:text-zinc-100 data-[highlighted]:text-zinc-100 data-[state=open]:text-violet-800">
                             New Incognito Window
                         </Menubar.Item>
                     </Menubar.Content>
@@ -33,15 +33,15 @@ export function MenubarDemo() {
                 </Menubar.Trigger>
                 <Menubar.Portal>
                     <Menubar.Content
-                        className="min-w-[220px] rounded-md bg-white p-[5px]"
+                        className="min-w-[220px] rounded-md bg-zinc-700 p-[5px]"
                         align="start"
                         sideOffset={5}
                         alignOffset={-3}
                     >
-                        <Menubar.Item className="group relative flex h-[25px] select-none items-center rounded px-2.5 leading-none text-violet-900 outline-none data-[disabled]:pointer-events-none data-[state=open]:bg-violet-50 data-[highlighted]:bg-gradient-to-br data-[highlighted]:from-violet-900 data-[highlighted]:to-violet-900 data-[disabled]:text-zinc-500 data-[highlighted]:data-[state=open]:text-violet-950 data-[highlighted]:text-violet-950 data-[state=open]:text-violet-900">
+                        <Menubar.Item className="group relative flex h-[34px] select-none items-center rounded px-2.5 leading-none text-zinc-100 outline-none data-[disabled]:pointer-events-none data-[state=open]:bg-violet-50 data-[highlighted]:bg-gradient-to-br data-[highlighted]:from-violet-500 data-[highlighted]:to-violet-800 data-[disabled]:text-zinc-600 data-[highlighted]:data-[state=open]:text-zinc-100 data-[highlighted]:text-zinc-100 data-[state=open]:text-violet-800">
                             Undo{" "}
                         </Menubar.Item>
-                        <Menubar.Item className="group relative flex h-[25px] select-none items-center rounded px-2.5 leading-none text-violet-900 outline-none data-[disabled]:pointer-events-none data-[state=open]:bg-violet-50 data-[highlighted]:bg-gradient-to-br data-[highlighted]:from-violet-900 data-[highlighted]:to-violet-900 data-[disabled]:text-zinc-500 data-[highlighted]:data-[state=open]:text-violet-950 data-[highlighted]:text-violet-950 data-[state=open]:text-violet-900">
+                        <Menubar.Item className="group relative flex h-[34px] select-none items-center rounded px-2.5 leading-none text-zinc-100 outline-none data-[disabled]:pointer-events-none data-[state=open]:bg-violet-50 data-[highlighted]:bg-gradient-to-br data-[highlighted]:from-violet-500 data-[highlighted]:to-violet-800 data-[disabled]:text-zinc-600 data-[highlighted]:data-[state=open]:text-zinc-100 data-[highlighted]:text-zinc-100 data-[state=open]:text-violet-800">
                             Redo{" "}
                         </Menubar.Item>
                     </Menubar.Content>
