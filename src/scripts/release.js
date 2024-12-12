c7a39f82f3c7ad45a0af192ca86df55a35e1b288
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2024-12-12 15:11:19 -0300

    script texts

--- src/scripts/release.js
+++ src/scripts/release.js
@@ -3,16 +3,13 @@ import { execSync } from 'child_process'
 import { existsSync, unlinkSync, writeFileSync } from 'fs'
 import { join } from 'path'
 
-// Lê o nome do pacote e o token
 const packageName = process.argv.find(arg => arg.startsWith('--package='))?.split('=')[1]
 const npmToken = process.env.NPM_TOKEN
 
-// Caminho do .npmrc
 const npmrcPath = join(process.cwd(), '.npmrc')
 
-// Verifica se o token está configurado
 if (!npmToken) {
-    console.error('Erro: NPM_TOKEN não definido.')
+    console.error('Error: NPM_TOKEN not defined.')
     process.exit(1)
 }
 
@@ -20,7 +17,7 @@ writeFileSync(npmrcPath, `//registry.npmjs.org/:_authToken=${npmToken}\n`)
 
 try {
     if (!packageName) {
-        console.log('Atualizando e publicando todos os pacotes...')
+        console.log('Updating and publishing all packages...')
         execSync('npm run build --workspaces', { stdio: 'inherit' })
         execSync('npm version patch --workspaces', { stdio: 'inherit' })
         execSync('npm run publish:all', { stdio: 'inherit' })
@@ -28,19 +25,19 @@ try {
         const packagePath = join(process.cwd(), 'src', 'packages', packageName)
 
         if (existsSync(packagePath)) {
-            console.log(`Atualizando e publicando o pacote: ${packageName}`)
+            console.log(`Updating and publishing package: ${packageName}`)
             execSync(`npm run build --workspace ${packagePath}`, { stdio: 'inherit' })
             execSync(`npm version patch --workspace ${packagePath}`, { stdio: 'inherit' })
             execSync(`npm publish --workspace ${packagePath} --if-present --access public`, { stdio: 'inherit' })
         } else {
-            console.error(`Erro: Pacote "${packageName}" não encontrado.`)
+            console.error(`Error: Package "${packageName}" not found.`)
         }
     }
 } catch (error) {
-    console.error('Erro ao executar o script:', error.message, error.stack)
+    console.error('Error executing the script:', error.message, error.stack)
 } finally {
     if (existsSync(npmrcPath)) {
         unlinkSync(npmrcPath)
-        console.log('.npmrc temporário removido.')
+        console.log('Temporary .npmrc removed.')
     }
 }
